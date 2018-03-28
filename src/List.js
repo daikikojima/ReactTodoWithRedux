56b59c8b9ea68ed129c977f20a21e12b8d8b6b52
Daiki Kojima <daikikojima0531@icloud.com>
2018-03-28 14:52:40 +0900

    src/List.js: add constructor

--- src/List.js
+++ src/List.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class List extends Component {
+    constructor(props) {
+        super(props)
+        this.deleteItem = this.deleteItem.bind(this)
+    }
+
     deleteItem(id) {
         this.props.delete(id)
     }
