5fdd9c30b20bf4524d647664a6bdc8e1b56d501c
Daiki Kojima <daikikojima0531@icloud.com>
2018-03-28 13:35:33 +0900

    src/App.js: make root components include 2 child components
    import 2 components describing below
    AddTodo: That enables us to add todo object
    List: showing us the list of todos.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddTodo from './AddTodo';
+import List from './List'
 
 class App extends Component {
   render() {
